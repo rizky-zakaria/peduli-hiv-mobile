5e7149920ea85a78b345948bb7e44ff8be5bfe9d
Rizky Zakaria <rizky.zakaria78@gmail.com>
2023-06-21 21:53:24 +0800

    --allow-empty

--- app/src/main/java/com/example/consult_app/utils/Config.java
+++ app/src/main/java/com/example/consult_app/utils/Config.java
@@ -1,6 +1,6 @@
 package com.example.consult_app.utils;
 
 public class Config {
-//    public static final String BASEURL = "https://792a-110-139-232-47.ap.ngrok.io/";
-    public static final String BASEURL = "http://peduli-hiv.bisadong.id/";
+    public static final String BASEURL = "https://89e9-36-85-225-226.ngrok-free.app/";
+//    public static final String BASEURL = "http://peduli-hiv.bisadong.id/";
 }
